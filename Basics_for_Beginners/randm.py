8fb0e7159230b16c2fe664d6270a7652d54293ae
BeatriceNzilani <158255737+BeatriceNzilani@users.noreply.github.com>
2025-04-18 00:37:00 +0300

    Update randm.py

--- Basics_for_Beginners/randm.py
+++ Basics_for_Beginners/randm.py
@@ -5,4 +5,10 @@ import random
 sequence = ("A", "B", "C", "D", "E")
 print(random.choice(sequence))       
 
-print(random.randrange(1, 5))       
+print(random.randrange(1, 5))  
+
+numbers = [1, 4, 9, 84, 585, 5434]
+print("Before shuffle:", numbers)
+
+random.shuffle(numbers)# shuffles
+print("After shuffle:", numbers)
