34e4d3666f9eafd585db43cc2f942fc18f3b3b2b
BeatriceNzilani <158255737+BeatriceNzilani@users.noreply.github.com>
2025-04-18 00:18:01 +0300

    basics of python

---
+++ Basics_for_Beginners/f_string.py
@@ -0,0 +1,8 @@
+#concatenate numbers and strings
+age =20 
+print(f"Njeri is {age} years.")
+
+print(f"He scored {3*20} out of 100.")
+
+price =90
+print (f"{price:.2f}")
---
+++ Basics_for_Beginners/randm.py
@@ -0,0 +1,8 @@
+
+
+import random
+
+sequence = ("A", "B", "C", "D", "E")
+print(random.choice(sequence))       
+
+print(random.randrange(1, 5))       
