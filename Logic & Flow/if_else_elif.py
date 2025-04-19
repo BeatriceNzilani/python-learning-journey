5ff1f41ed97e04f4c59cc4f98414d12986b2b520
BeatriceNzilani <158255737+BeatriceNzilani@users.noreply.github.com>
2025-04-19 15:17:48 +0300

    loops_concept

---
+++ Logic & Flow/if_else_elif.py
@@ -0,0 +1,54 @@
+x=30
+if x== 30 :
+    print("Average")
+
+y=30
+if y>= 40:
+    print("Good")
+else :
+    print("Can do better")    
+
+p=90
+if p<= 20:
+    print("Keep Trying")   
+elif p <= 30  and p>= 20:
+    print("Average") 
+elif p<= 40 and p>= 30 :
+    print("Good")   
+else:
+    print("Excellent")  
+
+#not
+c=5
+d=6
+if not c>d:
+    print("yes")
+
+# if caanot be empty and if in anycase its mpty use the "pass"
+g=5
+if g== 5:
+    pass
+
+""" nested if """# whe an if is within another if 
+g=7
+if g>5:
+    print("Above five,")
+    if g>10:
+        print("and also above 10.")
+    else:
+        print("and also less than 10.")     
+
+
+f=7
+t=8
+print("Hello") if t<7 else print("Goodbye")
+
+H=8
+D=28
+
+if H<D :print("ooh!")
+
+
+     
+
+
