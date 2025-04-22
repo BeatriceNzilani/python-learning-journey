a1be8578e32048ccd72f544e4c464e0516acd871
BeatriceNzilani <158255737+BeatriceNzilani@users.noreply.github.com>
2025-04-22 23:04:17 +0300

    Delete Basics_for_Beginners directory

--- Basics_for_Beginners/casting.py
+++
@@ -1,9 +0,0 @@
-# changing a datatype to another
-p=1.876
-print(int(p))
-
-p=str(1)
-print(type(p))
-
-p=5
-print(float(p))
--- Basics_for_Beginners/f_string.py
+++
@@ -1,8 +0,0 @@
-#concatenate numbers and strings
-age =20 
-print(f"Njeri is {age} years.")
-
-print(f"He scored {3*20} out of 100.")
-
-price =90
-print (f"{price:.2f}")
